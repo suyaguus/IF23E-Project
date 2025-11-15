f44d24dfb0cde2951dc6624f2cdfbbaaa1298a8a
Surya Agung <ellyglobal29@gmail.com>
2025-11-15 13:36:54 +0700

    commit service delete, put, get(id)

---
+++ api/src/app/api/user/[id]/route.ts
@@ -0,0 +1,125 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prisma";  // Sesuaikan dengan path Prisma Anda
+
+export const DELETE = async (
+    req: NextRequest,
+    context: { params: Promise<{ id: string }> }
+) => {
+    try {
+        const { id } = await context.params;
+        const userId = Number(id);
+
+        if (isNaN(userId)) {
+            return NextResponse.json(
+                { success: false, message: "id tidak valid" },
+            );
+        }
+
+        const user = await prisma.tb_user.findUnique({
+            where: { id: userId },
+        });
+
+        if (!user) {
+            return NextResponse.json(
+                { success: false, message: "user tidak ditemukan" },
+            );
+        }
+
+        await prisma.tb_user.delete({
+            where: { id: userId },
+        });
+
+        return NextResponse.json(
+            { success: true, message: "user berhasil dihapus" },
+        );
+
+    } catch (error) {
+        return NextResponse.json(
+            { success: false, message: "server error", error: String(error) },
+        );
+    }
+};
+
+// get user berdasarkan id
+export const GET = async (
+    req: NextRequest,
+    context: { params: Promise<{ id: string }> }
+) => {
+    try {
+        const { id } = await context.params;
+        const userId = Number(id);
+
+        if (isNaN(userId)) {
+            return NextResponse.json(
+                { success: false, message: "id tidak valid" },
+            );
+        }
+
+        const user = await prisma.tb_user.findUnique({
+            where: { id: userId },
+            select: {
+                username: true,
+                email: true,
+                password: true,
+                role: true
+            }
+        });
+
+        if (!user) {
+            return NextResponse.json(
+                { success: false, message: "user tidak ditemukan" },
+            );
+        }
+
+        return NextResponse.json(
+            { success: true, message: "Data berhasil ditemukan", user },
+        );
+
+    } catch (error) {
+        return NextResponse.json(
+            { success: false, message: "server error", error: String(error) },
+        );
+    }
+}
+
+// buat putt data
+export const PUT = async (
+    req: NextRequest,
+    context: { params: Promise<{ id: string }> }
+) => {
+    try {
+        const { id } = await context.params;
+        const userId = Number(id);
+        const data = await req.json();
+
+        if (isNaN(userId)) {
+            return NextResponse.json(
+                { success: false, message: "id tidak valid" },
+            );
+        }
+
+        const user = await prisma.tb_user.findUnique({
+            where: { id: userId },
+        });
+
+        if (!user) {
+            return NextResponse.json(
+                { success: false, message: "user tidak ditemukan" },
+            );
+        }
+
+        await prisma.tb_user.update({
+            where: { id: userId },
+            data: data,
+        });
+
+        return NextResponse.json(
+            { success: true, message: "Data berhasil diubah" },
+        );
+
+    } catch (error) {
+        return NextResponse.json(
+            { success: false, message: "server error", error: String(error) },
+        );
+    }
+}
