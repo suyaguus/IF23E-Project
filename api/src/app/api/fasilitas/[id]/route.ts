aa1c31ceffd4878ebea5d7f82e2cc685837e4d09
Surya Agung <ellyglobal29@gmail.com>
2025-11-16 15:25:34 +0700

    add fungsi delete update fasilitas/[id]/route.ts

---
+++ api/src/app/api/fasilitas/[id]/route.ts
@@ -0,0 +1,84 @@
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+// buat fungsi delete data
+export const DELETE = async (
+    req: NextRequest,
+    context: { params: Promise<{ id: string }> }
+) => {
+    try {
+        const { id } = await context.params;
+        const fasilitasId = Number(id);
+
+        if (isNaN(fasilitasId)) {
+            return NextResponse.json({
+                success: false,
+                message: "id tidak valid",
+            });
+        }
+
+        const fasilitas = await prisma.tb_fasilitas.findUnique({
+            where: { id: fasilitasId },
+        });
+
+        if (!fasilitas) {
+            return NextResponse.json({
+                success: false,
+                message: "fasilitas tidak ditemukan",
+            });
+        }
+
+        await prisma.tb_fasilitas.delete({
+            where: { id: fasilitasId },
+        });
+
+        return NextResponse.json({
+            success: true,
+            message: "fasilitas berhasil dihapus",
+        });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({
+            success: false,
+            message: "terjadi kesalahan",
+        });
+    }
+};
+
+// buat fungsi update data
+export const PUT = async (
+    req: NextRequest,
+    context: { params: Promise<{ id: string }> }
+) => {
+    const { id } = await context.params;
+    const fasilitasId = Number(id);
+    const data = await req.json();
+
+    if (isNaN(fasilitasId)) {
+        return NextResponse.json({
+            success: false,
+            message: "id tidak valid",
+        });
+    }
+
+    const fasilitas = await prisma.tb_fasilitas.findUnique({
+        where: { id: fasilitasId },
+    });
+
+    if (!fasilitas) {
+        return NextResponse.json({
+            success: false,
+            message: "fasilitas tidak ditemukan",
+        });
+    }
+
+    await prisma.tb_fasilitas.update({
+        where: { id: fasilitasId },
+        data: data,
+    });
+
+    return NextResponse.json({
+        success: true,
+        message: "fasilitas berhasil diupdate",
+    });
+};
