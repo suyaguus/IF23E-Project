4d518ae53daf9d9ec09c340c0b0bae7e3cd2503f
Surya Agung <ellyglobal29@gmail.com>
2025-11-16 16:26:21 +0700

    add delete service order/route.ts

---
+++ api/src/app/api/order/[id]/route.ts
@@ -0,0 +1,38 @@
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+// buat service delete data
+export const DELETE = async (
+    req: NextRequest,
+    context: {params: Promise<{id: string}>}
+) => {
+    const { id } = await context.params;
+    const orderId = Number(id);
+
+    if (isNaN(orderId)) {
+        return NextResponse.json({
+            success: false,
+            message: "id tidak valid",
+        });
+    }
+
+    const order = await prisma.tb_order.findUnique({
+        where: { id: orderId },
+    });
+
+    if (!order) {
+        return NextResponse.json({
+            success: false,
+            message: "order tidak ditemukan",
+        });
+    }
+
+    await prisma.tb_order.delete({
+        where: { id: orderId },
+    });
+
+    return NextResponse.json({
+        success: true,
+        message: "order berhasil dihapus",
+    });
+}
