5a080e84ba0ceaaaaa670554398c80340fdbcb02
Surya Agung <ellyglobal29@gmail.com>
2025-11-16 15:41:31 +0700

    add delete service fasilita/route.ts

---
+++ api/src/app/api/perabotan/[id]/route.ts
@@ -0,0 +1,38 @@
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+// buat service delete data
+export const DELETE = async (
+    req: NextRequest,
+    context: { params: Promise<{ id: string }> }
+) => {
+    const { id } = await context.params;
+    const perabotanId = Number(id);
+
+    if (isNaN(perabotanId)) {
+        return NextResponse.json({
+            success: false,
+            message: "id tidak valid",
+        });
+    }
+
+    const perabotan = await prisma.tb_perabotan.findUnique({
+        where: { id: perabotanId },
+    });
+
+    if (!perabotan) {
+        return NextResponse.json({
+            success: false,
+            message: "perabotan tidak ditemukan",
+        });
+    }
+
+    await prisma.tb_perabotan.delete({
+        where: { id: perabotanId },
+    });
+
+    return NextResponse.json({
+        success: true,
+        message: "perabotan berhasil dihapus",
+    });
+}
