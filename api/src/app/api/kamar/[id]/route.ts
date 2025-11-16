d3dd8e7c7fef45acd67e41ef5feb8cbb707484b2
Surya Agung <ellyglobal29@gmail.com>
2025-11-16 13:30:02 +0700

    add fungsi update kamar/[id]/route.ts

--- api/src/app/api/kamar/[id]/route.ts
+++ api/src/app/api/kamar/[id]/route.ts
@@ -1,6 +1,7 @@
 import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
+// fungsi untuk menghapus data
 export const DELETE = async (
     req: NextRequest,
     context: { params: Promise<{ id: string }> }
@@ -39,3 +40,45 @@ export const DELETE = async (
         });
     }
 };
+
+// buat fungsi update data
+export const PUT = async (
+    req: NextRequest,
+    context: { params: Promise<{ id: string }> }
+) => {
+    try {
+        const { id } = await context.params;
+        const kamarId = Number(id);
+        const data = await req.json();
+
+        if (isNaN(kamarId)) {
+            return NextResponse.json({
+                success: false, message: "id tidak valid"
+            });
+        }
+
+        const kamar = await prisma.tb_kamar.findUnique({
+            where: { id: kamarId }
+        });
+
+        if (!kamar) {
+            return NextResponse.json({
+                success: false, message: "kamar tidak ditemukan"
+            });
+        }
+
+        await prisma.tb_kamar.update({
+            where: { id: kamarId },
+            data: data
+        });
+
+        return NextResponse.json({
+            success: true, message: "kamar berhasil diupdate"
+        });
+
+    } catch (error) {
+        return NextResponse.json({
+            success: false, message: "terjadi kesalahan"
+        });
+    }
+}
