db82cfb7335a8a9f5965a19f6b60ab80ef26be13
Surya Agung <ellyglobal29@gmail.com>
2025-11-16 15:31:53 +0700

    add get service perabotan/route.ts

---
+++ api/src/app/api/perabotan/route.ts
@@ -0,0 +1,16 @@
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+// buat service tampil data
+export const GET = async() => {
+     const data = await prisma.tb_perabotan.findMany({
+          orderBy: {
+                id: 'asc'
+          }
+    });
+
+     return NextResponse.json({
+          perabotan: data
+     });
+};
+
