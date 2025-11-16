609764223cc6cee6a995e6f4b474554d9191a844
Surya Agung <ellyglobal29@gmail.com>
2025-11-16 13:04:14 +0700

    add file route.ts kamar

---
+++ api/src/app/api/kamar/route.ts
@@ -0,0 +1,55 @@
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async () => {
+    const data = await prisma.tb_kamar.findMany({
+        orderBy: {
+            id: "asc",
+        },
+    });
+
+    return NextResponse.json({
+        kamar: data,
+    });
+}
+
+// buat service simpan data
+export const POST = async (request: NextRequest) => {
+
+    // buat dalam format json
+    const data = await request.json();
+
+    // mengecek apakah data kamar sudah ada 
+    const check = await prisma.tb_kamar.findFirst({
+        where: {
+            nomorKamar: data.nomorKamar
+        },
+        select: {
+            nomorKamar: true
+        }
+    })
+
+    // jika nomor kamar ditemukan
+    if (check) {
+        return NextResponse.json({
+            message: "Data Kamar Gagal Disimpan ! (Nomor Kamar sudah digunakan)",
+            success: false
+        })
+    }
+
+    // jika nomor kamar tidak ditemukan
+    await prisma.tb_kamar.create({
+        data: {
+            nomorKamar: data.nomorKamar,
+            hargaSewa: data.hargaSewa,
+            statusKamar: data.statusKamar || "Tersedia",
+            deskripsi: data.deskripsi
+        }
+    })
+
+    // tampilkan respon
+    return NextResponse.json({
+        message: "Data Kamar Berhasil Disimpan !",
+        success: true
+    })
+}
