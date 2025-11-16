c438ced15bb006341345a3317f96f2953a0d857c
Surya Agung <ellyglobal29@gmail.com>
2025-11-16 14:38:07 +0700

    add fungsi post fasilitas/route.ts

--- api/src/app/api/fasilitas/route.ts
+++ api/src/app/api/fasilitas/route.ts
@@ -13,3 +13,42 @@ export const GET = async() => {
         fasilitas: data
     })
 };
+
+// buat service simpan data
+export const POST = async (request: NextRequest) => {
+
+    // buat fungsi dalam format json
+    const data = await request.json();
+
+    // mengecek apakah data fasilitas sudah ada 
+    const check = await prisma.tb_fasilitas.findFirst({
+        where: {
+            namaFasilitas: data.namaFasilitas
+        },
+        select: {
+            namaFasilitas: true
+        }
+    })
+
+    // jika faslitias sudah ditemukan
+    if (check) {
+        return NextResponse.json({
+            message: "Data Gagal Disimpan ! (Fasilitas Sudah Ada)",
+            success: false
+        })
+    }
+
+    // jika fasilitas tidak ditemukan
+    await prisma.tb_fasilitas.create({
+        data: {
+            namaFasilitas: data.namaFasilitas,
+            deskripsi: data.deskripsi
+        }
+    })
+
+    // tampilkan respon
+    return NextResponse.json({
+        message: "Data Berhasil Disimpan !",
+        success: true
+    })
+}
