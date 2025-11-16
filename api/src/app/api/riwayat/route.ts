88a0a22aadf060306a28d704fb535839a0048b5d
Surya Agung <ellyglobal29@gmail.com>
2025-11-16 16:52:05 +0700

    add post service riwayat/route.ts

--- api/src/app/api/riwayat/route.ts
+++ api/src/app/api/riwayat/route.ts
@@ -12,3 +12,43 @@ export const GET = async() => {
         riwayat: data
     })
 }
+
+export const POST = async (request: NextRequest) => {
+    const data = await request.json();
+
+    // Cek duplikasi menggunakan kodeRiwayat (bukan id)
+    const check = await prisma.tb_riwayat_pembayaran.findUnique({
+        where: {
+            kodeRiwayat: data.kodeRiwayat
+        }
+    })
+
+    if (check) {
+        return NextResponse.json({
+            message: "Data Gagal Disimpan ! (Riwayat Sudah Ada)",
+            success: false
+        })
+    }
+
+    // Tambahkan orderId, userId, kamarId yang required
+    await prisma.tb_riwayat_pembayaran.create({
+        data: {
+            kodeRiwayat: data.kodeRiwayat,
+            orderId: data.orderId,
+            userId: data.userId,
+            kamarId: data.kamarId,
+            statusPembayaranLama: data.statusPembayaranLama,
+            statusPembayaranBaru: data.statusPembayaranBaru,
+            totalHarga: data.totalHarga,
+            metodePembayaran: data.metodePembayaran,
+            buktiPembayaran: data.buktiPembayaran,
+            keterangan: data.keterangan,
+            diubahOleh: data.diubahOleh
+        }
+    })
+
+    return NextResponse.json({
+        message: "Data Berhasil Disimpan !",
+        success: true
+    })
+}
