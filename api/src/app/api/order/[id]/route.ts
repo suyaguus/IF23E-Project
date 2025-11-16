d66c54cec906107169e1a484a2bbe8674c8f0742
Surya Agung <ellyglobal29@gmail.com>
2025-11-16 16:34:32 +0700

    add get by id service order/route.ts

--- api/src/app/api/order/[id]/route.ts
+++ api/src/app/api/order/[id]/route.ts
@@ -74,3 +74,35 @@ export const PUT = async (
         message: "order berhasil diupdate",
     });
 }
+
+// buat service get by id
+export const GET = async (
+    req: NextRequest,
+    context: { params: Promise<{ id: string }> }
+) => {
+    const { id } = await context.params;
+    const orderId = Number(id);
+
+    if (isNaN(orderId)) {
+        return NextResponse.json({
+            success: false,
+            message: "id tidak valid",
+        });
+    }
+
+    const order = await prisma.tb_order.findUnique({
+        where: { id: orderId },
+    });
+
+    if (!order) {
+        return NextResponse.json({
+            success: false,
+            message: "order tidak ditemukan",
+        });
+    }
+
+    return NextResponse.json({
+        success: true,
+        data: order,
+    });
+}
