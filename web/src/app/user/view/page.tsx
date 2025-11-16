163194e80533f01c63e8a46e0f352f077a175654
Surya Agung <ellyglobal29@gmail.com>
2025-11-17 01:14:53 +0700

    add table & view/page.tsx

---
+++ web/src/app/user/view/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function UserViewPage() {
+    return (
+        <>
+            {/* tambah data */}
+
+            {/* article */}
+            <article>
+                
+            </article>
+        </>
+    )
+}
