d5ff311ddb6d5cd148ec68f4880a745ece33d7e4
Surya Agung <ellyglobal29@gmail.com>
2025-11-16 19:34:02 +0700

    add delete kamarFasilitas/route.ts

--- api/src/app/api/kamarFasilitas/route.ts
+++ api/src/app/api/kamarFasilitas/route.ts
@@ -76,3 +76,30 @@ export const POST = async (req: NextRequest) => {
         });
     }
 };
+
+
+// service delete
+export const DELETE = async (req: NextRequest) => {
+    try {
+        const { kamarId, fasilitasId } = await req.json();
+
+        await prisma.tb_kamar_fasilitas.delete({
+            where: {
+                kamarId_fasilitasId: {
+                    kamarId: Number(kamarId),
+                    fasilitasId: Number(fasilitasId)
+                }
+            }
+        });
+
+        return NextResponse.json({
+            success: true,
+            message: "Fasilitas berhasil dihapus dari kamar"
+        });
+    } catch {
+        return NextResponse.json({
+            success: false,
+            message: "Server error"
+        });
+    }
+}
