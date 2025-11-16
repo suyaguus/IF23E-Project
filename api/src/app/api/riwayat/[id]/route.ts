64dd5515ee46d543487ab9ce1bbea68cfb0d3545
Surya Agung <ellyglobal29@gmail.com>
2025-11-16 18:51:20 +0700

    add get by id riwayat/[id]/route.ts

--- api/src/app/api/riwayat/[id]/route.ts
+++ api/src/app/api/riwayat/[id]/route.ts
@@ -75,3 +75,35 @@ export const PUT = async (
         data: data  
     })
 }
+
+// buat get berdasarkan id
+export const GET = async (
+    req: NextRequest,
+    context: { params: Promise<{ id: string }> }
+) => {
+    const { id } = await context.params;
+    const riwayatId = Number(id);
+
+    if (isNaN(riwayatId)) {
+        return NextResponse.json({
+            success: false,
+            message: "id tidak valid",
+        });
+    }
+
+    const riwayat = await prisma.tb_riwayat_pembayaran.findUnique({
+        where: { id: riwayatId },
+    });
+
+    if (!riwayat) {
+        return NextResponse.json({
+            success: false,
+            message: "riwayat tidak ditemukan",
+        });
+    }
+
+    return NextResponse.json({
+        success: true,
+        data: riwayat
+    })
+}
