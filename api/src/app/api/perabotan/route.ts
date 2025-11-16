163455d0fde1722f71059f07be34d859ed027b11
Surya Agung <ellyglobal29@gmail.com>
2025-11-16 15:36:31 +0700

    add post service fasilita/route.ts

--- api/src/app/api/perabotan/route.ts
+++ api/src/app/api/perabotan/route.ts
@@ -14,3 +14,42 @@ export const GET = async() => {
      });
 };
 
+// buat service simpan data
+export const POST = async (request: NextRequest) => {
+
+     // buat dalam format json
+     const data = await request.json();
+
+     // mengecek apakah data perabotan sudah ada 
+     const check = await prisma.tb_perabotan.findFirst({
+          where: {
+               namaPerabotan: data.namaPerabotan
+          },
+          select: {
+               namaPerabotan: true
+          }
+     })
+
+     // jika perabotan sudah ditemukan
+     if (check) {
+          return NextResponse.json({
+               message: "Data Gagal Disimpan ! (Perabotan Sudah Ada)",
+               success: false
+          })
+     }
+
+     // jika perabotan tidak ditemukan
+     await prisma.tb_perabotan.create({
+          data: {
+               namaPerabotan: data.namaPerabotan,
+               kodePerabotan: data.kodePerabotan,
+               deskripsi: data.deskripsi
+          }
+     });
+
+    //  tampilkan respon
+     return NextResponse.json({
+          message: "Data Berhasil Disimpan !",
+          success: true
+     });
+}
