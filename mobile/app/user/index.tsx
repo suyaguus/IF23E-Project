15b3920130eb197e1ed3f68d1d7c2c0f697cb7d0
Surya Agung <ellyglobal29@gmail.com>
2025-11-16 23:33:01 +0700

    start editing user/index.tsx

--- mobile/app/user/index.tsx
+++ mobile/app/user/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
-import { View } from 'react-native-reanimated/lib/typescript/Animated';
+import { Text, View } from 'react-native';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
 
 export default function UserViewPage() {
     return (
         <View>
+            {/* Area Header */}
+            <Text>
+                Halaman View User
+            </Text>
+
+            {/* Area Content */}
+
+            {/* Area FAB */}
+
             
         </View>
     )
