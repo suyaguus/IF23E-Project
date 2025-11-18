285afeacb4b2742d05557eceb38045a26e0bec4b
Surya Agung <ellyglobal29@gmail.com>
2025-11-18 15:39:18 +0700

    add components label & input in user/add

--- web/src/app/user/add/page.tsx
+++ web/src/app/user/add/page.tsx
@@ -1,13 +1,28 @@
 import Image from "next/image";
 import styles from "../user.module.css";
 import React from "react";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 
 export default function UserAddPage() {
-    return (
-        <>
-            <article>
-                testing
-            </article>
-        </>
-    )
+  return (
+    <>
+      <article>
+        
+        {/* area kode */}
+        <section>
+          <div className="grid w-full max-w-sm items-center gap-3">
+            {/* fungsi htmlfor mengarahkan label ke input walaupun terpisah */}
+            <Label htmlFor="txt_kode">Username</Label>
+            <Input
+              type="text"
+              id="txt_kode"
+              placeholder="Isi Username"
+              maxLength={15}
+            />
+          </div>
+        </section>
+      </article>
+    </>
+  );
 }
