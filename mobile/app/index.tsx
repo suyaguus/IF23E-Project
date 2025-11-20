d9260d3db4e3ca3a984f3a0aa77a91174693f1bb
Surya Agung <ellyglobal29@gmail.com>
2025-11-21 01:08:28 +0700

    fixed index.tsx

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,9 +1,5 @@
 import React from 'react'
-<<<<<<< HEAD
 import HomePage from './Home/page'
-=======
-import UserViewPage from './Home/page'
->>>>>>> 0c51ec5dcc7cd74e3a815dda9a37a8a28410ddb0
 
 export default function Home() {
     return (
