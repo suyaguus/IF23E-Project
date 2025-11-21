9ad022a47fb66bbc288e7f966d5862e60cc0be34
Surya Agung <ellyglobal29@gmail.com>
2025-11-21 09:25:52 +0700

    add export function

---
+++ web/src/app/perabotan/edit/[id]/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import EditDataPage from "@/components/edit-data-page";
+
+export default function EditPerabotanPage() {
+  return (
+    <EditDataPage
+      dataType="perabotan"
+      redirectPath="/perabotan"
+      title="Edit Data Perabotan"
+    />
+  );
+}
