f6a3062d1c591b51aaa18a3639ffc839e6e55c12
Surya Agung <ellyglobal29@gmail.com>
2025-11-21 09:16:17 +0700

    editing kamar/edit/[id]/page.tsx

---
+++ web/src/app/kamar/edit/[id]/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import EditDataPage from "@/components/edit-data-page";
+
+export default function EditKamarPage() {
+  return (
+    <EditDataPage
+      dataType="kamar"
+      redirectPath="/kamar"
+      title="Edit Data Kamar"
+    />
+  );
+}
