00d5f4eb0743b3d3c6d07543977baffeaf6fbb55
Surya Agung <ellyglobal29@gmail.com>
2025-11-21 09:27:47 +0700

    add export funtion

---
+++ web/src/app/fasilitas/edit/[id]/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import EditDataPage from "@/components/edit-data-page";
+
+export default function EditFasilitasPage() {
+  return (
+    <EditDataPage
+      dataType="fasilitas"
+      redirectPath="/fasilitas"
+      title="Edit Data Fasilitas"
+    />
+  );
+}
