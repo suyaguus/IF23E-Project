3d4e73a1a3db799e9fddddaba48610efa166fcd6
Surya Agung <ellyglobal29@gmail.com>
2025-11-24 23:15:58 +0700

    restart project mobile

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,52 +1,24 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
-import { Stack } from "expo-router";
-import "react-native-reanimated";
-import { PaperProvider } from "react-native-paper";
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
 
-import { useColorScheme } from "@/hooks/use-color-scheme";
-import { StatusBar } from "react-native";
+import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export const unstable_settings = {
-  anchor: "(tabs)",
-};
-
-const theme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: "#FF0000",
-    secondary: "#FFCC00",
-  },
+  anchor: '(tabs)',
 };
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    // <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-    //   <Stack>
-
-    //     {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    //     <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} /> */}
-
-    //   </Stack>
-    //   <StatusBar style="auto" />
-    // </ThemeProvider>
-
-    <PaperProvider>
-      <Stack
-        screenOptions={{
-          // menghilangkan header Index
-          headerShown: false,
-        }}
-      ></Stack>
-
-      {/* buat status bar */}
-      <StatusBar barStyle={"light-content"} backgroundColor={"#a51c31"} />
-    </PaperProvider>
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
   );
 }
--- mobile/app/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import AdminPage from './Admin/page'
-import HomePage from './Home/page'
-
-export default function Admin() {
-    return (
-        <HomePage/>
-    )
-}
