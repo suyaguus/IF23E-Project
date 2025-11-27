f3779758e4a76042f790fd60ec11a88b5355a9d1
Surya Agung <ellyglobal29@gmail.com>
2025-11-27 22:12:04 +0700

    fixed api/src/app/api/fasilitas/route.ts

--- api/src/app/api/fasilitas/route.ts
+++ api/src/app/api/fasilitas/route.ts
@@ -2,16 +2,21 @@ import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 // buat service tampil data
-export const GET = async() => {
+export const GET = async () => {
     const data = await prisma.tb_fasilitas.findMany({
         orderBy: {
             id: 'asc'
         }
     })
 
-    return NextResponse.json({
-        fasilitas: data
-    })
+    return NextResponse.json(
+        {
+            fasilitas: data
+        },
+        {
+            status: 200
+        }
+    )
 };
 
 // buat service simpan data
@@ -32,10 +37,15 @@ export const POST = async (request: NextRequest) => {
 
     // jika faslitias sudah ditemukan
     if (check) {
-        return NextResponse.json({
-            message: "Data Gagal Disimpan ! (Fasilitas Sudah Ada)",
-            success: false
-        })
+        return NextResponse.json(
+            {
+                message: "Data Gagal Disimpan ! (Fasilitas Sudah Ada)",
+                success: false
+            },
+            {
+                status: 400
+            }
+        )
     }
 
     // jika fasilitas tidak ditemukan
@@ -48,8 +58,13 @@ export const POST = async (request: NextRequest) => {
     })
 
     // tampilkan respon
-    return NextResponse.json({
-        message: "Data Berhasil Disimpan !",
-        success: true
-    })
+    return NextResponse.json(
+        {
+            message: "Data Berhasil Disimpan !",
+            success: true
+        },
+        {
+            status: 200
+        }
+    )
 }
