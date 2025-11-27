7d9639107a32b568e3478ed1031feef87140a7be
Surya Agung <ellyglobal29@gmail.com>
2025-11-27 22:32:31 +0700

    editing styles

--- mobile/styles/home.ts
+++ mobile/styles/home.ts
@@ -3,5 +3,19 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
     warna_bg: {
         backgroundColor: "#0046FF",
-    }
+    },
+    screen: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    customItem: {
+        padding: 15,
+        backgroundColor: "#ddd",
+        marginTop: 10,
+    },
+    customText: {
+        fontSize: 18,
+        color: "#000",
+    },
 })
