131bb763032efe68665f9a7a38803ea72e98b7d1
Surya Agung <ellyglobal29@gmail.com>
2025-11-28 10:55:19 +0700

    add dropdown statusPembayaran, styles dropdown

--- mobile/app/kamar/add/page.tsx
+++ mobile/app/kamar/add/page.tsx
@@ -1,6 +1,6 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React, { useState } from "react";
-import { styles } from "@/styles/dashboard"; // import styles dari file styles/barang.ts
+import { styles } from "@/styles/dashboard";
 import { Button, TextInput } from "react-native-paper";
 import { router } from "expo-router";
 import {
@@ -9,6 +9,8 @@ import {
   filterNomorKamar,
   formatRibuan,
 } from "@/utils/script";
+import { Dropdown } from "react-native-element-dropdown";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 // testing dorpdown
 const statusPembayaran = [
@@ -16,6 +18,10 @@ const statusPembayaran = [
   { label: "Tersewa", value: "Tersewa" },
   { label: "TidakTersedia", value: "TidakTersedia" },
 ];
+interface DropdownItem {
+  label: string;
+  value: number;
+}
 
 export default function AddKamarPage() {
   // buat state
@@ -24,6 +30,30 @@ export default function AddKamarPage() {
 
   const [formHarga, setFormHarga] = useState("");
   const [formHargaRaw, setFormHargaRaw] = useState(0);
+  // bagian useState untuk satuan
+  const [value, setValue] = useState(null);
+
+  const renderItem = (item: DropdownItem) => {
+    return (
+      <View style={styles_dropdown.item}>
+        <Text style={styles_dropdown.textItem}>{item.label}</Text>
+        {item.value === value && (
+          // <AntDesign
+          //   style={styles.icon}
+          //   color="black"
+          //   name="Safety"
+          //   size={20}
+          // />
+          <MaterialIcons
+            name="search"
+            style={styles_dropdown.icon}
+            size={24}
+            color="black"
+          />
+        )}
+      </View>
+    );
+  };
 
   return (
     <View
@@ -65,6 +95,39 @@ export default function AddKamarPage() {
       />
 
       {/* area status kamar */}
+      <Dropdown
+        style={styles_dropdown.dropdown}
+        placeholderStyle={styles_dropdown.placeholderStyle}
+        selectedTextStyle={styles_dropdown.selectedTextStyle}
+        inputSearchStyle={styles_dropdown.inputSearchStyle}
+        iconStyle={styles_dropdown.iconStyle}
+        data={statusPembayaran}
+        search
+        maxHeight={300}
+        labelField="label"
+        valueField="value"
+        placeholder="Pilih Satuan"
+        searchPlaceholder="Pilih..."
+        value={value}
+        onChange={(item) => {
+          setValue(item.value);
+        }}
+        renderLeftIcon={() => (
+          // <AntDesign
+          //   style={styles_dropdown.icon}
+          //   color="black"
+          //   name="Safety"
+          //   size={20}
+          // />
+          <MaterialIcons
+            name="keyboard-arrow-down"
+            style={styles_dropdown.icon}
+            size={24}
+            color="black"
+          />
+        )}
+        renderItem={renderItem}
+      />
 
       {/* area deskripsi */}
       <TextInput
@@ -77,7 +140,6 @@ export default function AddKamarPage() {
       {/* area tombol */}
       <View
         style={{
-          flexDirection: "row",
           justifyContent: "flex-end",
           marginTop: 10,
           gap: 10,
@@ -92,6 +154,14 @@ export default function AddKamarPage() {
               `${formKamar}, ${formDeskripsi}, ${formHarga}, ${formHargaRaw}`
             )
           }
+          style={{
+            marginTop: 20,
+            backgroundColor: "#0046FF",
+            paddingVertical: 8,
+            marginRight: 20,
+            marginLeft: 20,
+          }}
+          labelStyle={{ fontSize: 16, color: "#fff" }}
         >
           Simpan
         </Button>
@@ -99,7 +169,14 @@ export default function AddKamarPage() {
           icon="close"
           mode="outlined"
           onPress={() => router.push("/kamar/page")}
-          style={{ marginRight: 10 }}
+          style={{
+            marginTop: 5,
+            borderColor: "#0046FF",
+            paddingVertical: 8,
+            marginRight: 20,
+            marginLeft: 20,
+          }}
+          labelStyle={{ fontSize: 16, color: "#0046FF" }}
         >
           Batal
         </Button>
@@ -107,3 +184,49 @@ export default function AddKamarPage() {
     </View>
   );
 }
+
+const styles_dropdown = StyleSheet.create({
+  dropdown: {
+    margin: 0,
+    height: 50,
+    backgroundColor: "white",
+    borderRadius: 0,
+    padding: 12,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+
+    elevation: 2,
+  },
+  icon: {
+    marginRight: 5,
+  },
+  item: {
+    padding: 17,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  textItem: {
+    flex: 1,
+    fontSize: 16,
+  },
+  placeholderStyle: {
+    fontSize: 16,
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
+  },
+});
