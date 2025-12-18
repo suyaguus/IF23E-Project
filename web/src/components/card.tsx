3545428729c8264d32b45ed147bc57be00c99013
Surya Agung Firdaus <ellyglobal29@gmail.com>
2025-12-18 11:12:32 +0700

    feat: fetching data components/card.tsx

--- web/src/components/card.tsx
+++ web/src/components/card.tsx
@@ -65,17 +65,14 @@ export function Cards() {
   return (
     <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
       {/* Card Users */}
-      <Card className="@container/card">
+      {/* <Card className="@container/card">
         <CardHeader>
           <CardDescription>Total Users</CardDescription>
           <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
             {getCardValue(userLoading, userError, totalUsers)}
           </CardTitle>
           <CardAction>
-            {/* <Badge variant="outline">
-              <IconTrendingUp />
-              +12.5%
-            </Badge> */}
+            
           </CardAction>
         </CardHeader>
         <CardFooter className="flex-col items-start gap-1.5 text-sm">
@@ -84,7 +81,7 @@ export function Cards() {
           </div>
           <div className="text-muted-foreground">Total Pengguna Aplikasi</div>
         </CardFooter>
-      </Card>
+      </Card> */}
 
       {/* Card Kamar */}
       <Card className="@container/card">
@@ -126,7 +123,9 @@ export function Cards() {
           <div className="line-clamp-1 flex gap-2 font-medium">
             <IconFridge className="size-4" />
           </div>
-          <div className="text-muted-foreground">Total Fasilitas yang tersedia</div>
+          <div className="text-muted-foreground">
+            Total Fasilitas yang tersedia
+          </div>
         </CardFooter>
       </Card>
 
@@ -148,7 +147,9 @@ export function Cards() {
           <div className="line-clamp-1 flex gap-2 font-medium">
             <IconArmchair className="size-4" />
           </div>
-          <div className="text-muted-foreground">Total Perabotan yang tersedia</div>
+          <div className="text-muted-foreground">
+            Total Perabotan yang tersedia
+          </div>
         </CardFooter>
       </Card>
     </div>
