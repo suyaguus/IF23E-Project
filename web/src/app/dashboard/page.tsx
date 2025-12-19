b1b5608a1aea19cbd4b91b284b70c90300439c89
Fito016 <alfitof919@gmail.com>
2025-12-19 16:57:35 +0700

    feat: add SidebarProvider

--- web/src/app/dashboard/page.tsx
+++ web/src/app/dashboard/page.tsx
@@ -1,5 +1,38 @@
+import { AppSidebardashboard } from "@/components/app-sidebardashboard";
+import { SiteHeader } from "@/components/site-header";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { Cards } from "@/components/card"
+
 export default function DashBoardPage() {
     return (
+        <SidebarProvider
+            style={
+                {
+                    "--sidebar-width": "calc(var(--spacing) * 72)",
+                    "--header-height": "calc(var(--spacing) * 12)",
+                } as React.CSSProperties
+            }
+        >
+            <AppSidebardashboard />
+            <SidebarInset>
+                <SiteHeader />
+                <div className="flex flex-1 flex-col">
+                    <div className="@container/main flex flex-1 flex-col gap-2">
+                        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+                            {/* bagian card */}
+                            <Cards />
+                            {/* bagian section data */}
+                            {/* <SectionCards /> */}
+                            {/* <div className="px-4 lg:px-6">
+                <ChartAreaInteractive />
+              </div> */}
 
-    );
+                        </div>
+                    </div>
+                </div>
+
+            </SidebarInset>
+        </SidebarProvider>
+
+    )
 }
