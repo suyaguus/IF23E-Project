8044b43b9b406eea0f7ba879d8a2b9334dfc1d9e
Fito016 <alfitof919@gmail.com>
2025-12-19 15:49:22 +0700

    feat: add buttonsidebar

--- web/src/components/nav-guest.tsx
+++ web/src/components/nav-guest.tsx
@@ -12,7 +12,13 @@ import {
   IconUser,
   type Icon,
 } from "@tabler/icons-react";
-
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
 import { Button } from "@/components/ui/button";
 
 import Link from "next/link";
@@ -27,6 +33,21 @@ export function NavGuest({
   }[];
 }) {
   return (
+     <SidebarGroup>
+      <SidebarGroupContent className="flex flex-col gap-2">
+        
+        <SidebarMenu>
+          {items.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton tooltip={item.title}>
+                {item.icon && <item.icon />}
+                <span>{item.title}</span>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
    
   );
 }
