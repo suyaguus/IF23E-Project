0d692e953ea6bcd063905f9970f82b8824219626
Fito016 <alfitof919@gmail.com>
2025-12-19 16:00:54 +0700

    feat: add state untuk user dengan default guest

--- web/src/components/app-sidebardashboard.tsx
+++ web/src/components/app-sidebardashboard.tsx
@@ -1,6 +1,19 @@
+import { useState } from "react";
+import { Sidebar } from "./ui/sidebar";
+
 const data = {
     user: {
 
     },
     navGuest: [],
+};
+
+export function AppSidebardashboard({ ...props }: React.ComponentProps<typeof Sidebar>) {
+    // Buat State untuk User dengan Default "Guest"
+  const [currentUser, setCurrentUser] = useState({
+    name: "Guest User",
+    email: "guest@example.com",
+    avatar: "", // Kosongkan atau isi path gambar default
+  });
+  
 }
