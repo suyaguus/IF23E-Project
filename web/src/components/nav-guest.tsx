235b4a633435354cae7f1718cdc8658f538366d1
Fito016 <alfitof919@gmail.com>
2025-12-19 15:34:22 +0700

    feat: add function NavGuest

---
+++ web/src/components/nav-guest.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import {
+  IconArmchair,
+  IconBed,
+  IconChairDirector,
+  IconCirclePlusFilled,
+  IconDashboard,
+  IconFridge,
+  IconMail,
+  IconMenu,
+  IconUser,
+  type Icon,
+} from "@tabler/icons-react";
+
+import { Button } from "@/components/ui/button";
+
+import Link from "next/link";
+
+export function NavGuest({
+  items,
+}: {
+  items: {
+    title: string;
+    url: string;
+    icon?: Icon;
+  }[];
+}) {
+  return (
+   
+  );
+}
