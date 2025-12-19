4eb6894ed88e19335aba5fa4ef4b24a8ef542273
Fito016 <alfitof919@gmail.com>
2025-12-19 15:50:50 +0700

    feat: add menusidebar

--- web/src/components/nav-guest.tsx
+++ web/src/components/nav-guest.tsx
@@ -1,53 +1,66 @@
 "use client";
 
 import {
-  IconArmchair,
-  IconBed,
-  IconChairDirector,
-  IconCirclePlusFilled,
-  IconDashboard,
-  IconFridge,
-  IconMail,
-  IconMenu,
-  IconUser,
-  type Icon,
+    IconArmchair,
+    IconBed,
+    IconChairDirector,
+    IconCirclePlusFilled,
+    IconDashboard,
+    IconFridge,
+    IconMail,
+    IconMenu,
+    IconUser,
+    type Icon,
 } from "@tabler/icons-react";
 import {
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
 } from "@/components/ui/sidebar";
 import { Button } from "@/components/ui/button";
 
 import Link from "next/link";
 
 export function NavGuest({
-  items,
+    items,
 }: {
-  items: {
-    title: string;
-    url: string;
-    icon?: Icon;
-  }[];
+    items: {
+        title: string;
+        url: string;
+        icon?: Icon;
+    }[];
 }) {
-  return (
-     <SidebarGroup>
-      <SidebarGroupContent className="flex flex-col gap-2">
-        
-        <SidebarMenu>
-          {items.map((item) => (
-            <SidebarMenuItem key={item.title}>
-              <SidebarMenuButton tooltip={item.title}>
-                {item.icon && <item.icon />}
-                <span>{item.title}</span>
-              </SidebarMenuButton>
-            </SidebarMenuItem>
-          ))}
-        </SidebarMenu>
-      </SidebarGroupContent>
-    </SidebarGroup>
-   
-  );
+    return (
+        <SidebarGroup>
+            <SidebarGroupContent className="flex flex-col gap-2">
+
+                <SidebarMenu>
+                    {/* bagian judul */}
+                    <SidebarMenuItem className="flex items-center gap-2">
+                        <SidebarMenuButton
+                            tooltip="Menu Sidebar"
+                            className="bg-transparent text-black hover:bg-transparent hover:text-black min-w-8 duration-200 ease-linear"
+                        >
+                            <IconMenu />
+                            <span>Menu Sidebar</span>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+
+                </SidebarMenu>
+                <SidebarMenu>
+                    {items.map((item) => (
+                        <SidebarMenuItem key={item.title}>
+                            <SidebarMenuButton tooltip={item.title}>
+                                {item.icon && <item.icon />}
+                                <span>{item.title}</span>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    ))}
+                </SidebarMenu>
+            </SidebarGroupContent>
+        </SidebarGroup>
+
+    );
 }
