c67068d0f86f0624313fafe9b5e3f6dab6eeaea2
Surya Agung Firdaus <ellyglobal29@gmail.com>
2025-12-19 18:58:40 +0700

    fix: fix layout

--- mobile/app/auth/_layout.tsx
+++ mobile/app/auth/_layout.tsx
@@ -5,16 +5,16 @@ export default function AuthLayout() {
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="login/index" />
       <Stack.Screen name="signup/index" />
-      <Stack.Screen name="forgotPassword/index" />
+      <Stack.Screen name="forgot-password/index" />
 
       {/* Pastikan nama ini sesuai dengan folder fisik Anda */}
       {/* Jika foldernya 'verifyOtp', gunakan verifyOtp/index */}
-      <Stack.Screen name="verifyOtp/index" />
+      <Stack.Screen name="verify-otp/index" />
 
       {/* Jika foldernya 'verify-otp', gunakan verify-otp/index */}
       {/* <Stack.Screen name="verify-otp/index" /> */}
 
-      <Stack.Screen name="resetPassword/index" />
+      <Stack.Screen name="reset-password/index" />
     </Stack>
   );
 }
