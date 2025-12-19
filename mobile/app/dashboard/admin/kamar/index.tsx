4178d498017da5dbee16f982a782b002f3f4a9f7
Surya Agung Firdaus <ellyglobal29@gmail.com>
2025-12-19 18:18:34 +0700

    feat: add file admin/kamar

---
+++ mobile/app/dashboard/admin/kamar/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { View, Text } from "react-native";
+export default function KamarPage() {
+  return (
+    <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+      <Text>Halaman Kelola Kamar</Text>
+    </View>
+  );
+}
