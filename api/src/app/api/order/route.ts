52c84e34eab14fae6fc3a274b0876f94be8b87f0
Surya Agung Firdaus <ellyglobal29@gmail.com>
2025-12-22 15:18:36 +0700

    fix: fix code

--- api/src/app/api/order/route.ts
+++ api/src/app/api/order/route.ts
@@ -1,13 +1,17 @@
 import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
+// buat service tampil data
 export const GET = async () => {
+
+    // ambil data
     const data = await prisma.tb_order.findMany({
         orderBy: {
             id: 'asc'
         }
     });
 
+    // tampilkan response
     return NextResponse.json(
         {
             order: data
@@ -24,6 +28,7 @@ export const POST = async (request: NextRequest) => {
     // buat dalam format json
     const data = await request.json();
 
+    // mengecek apakah data order sudah ada
     const check = await prisma.tb_order.findFirst({
         where: {
             kodeOrder: data.kodeOrder
