9548ec20e96ea3a16358d6e5492d9a9450ba0fed
Surya Agung Firdaus <ellyglobal29@gmail.com>
2025-12-25 12:09:51 +0700

    deleted file

--- web/src/lib/fetchers/usersFetcher.ts
+++
@@ -1,5 +0,0 @@
-import { api } from "@/lib/api";
-import { User } from "@/types/user";
-
-export const usersFetcher = (url: string): Promise<User[]> =>
-    api.get(url).then(res => res.data.users);
