08f7b8f6d9a703e58040b5114a1148cd34c9a630
Surya Agung Firdaus <ellyglobal29@gmail.com>
2025-12-25 14:23:48 +0700

    fix import use auth

--- web/src/components/nav-user.tsx
+++ web/src/components/nav-user.tsx
@@ -35,7 +35,7 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
-import { useAuth } from "@/context/AuthContext";
+import { useAuth } from "@/hooks/useAuth";
 import { useRouter } from "next/navigation";
 import { redirect } from "next/navigation";
 
