b6ed4f45abdd51a9278ca09e062c21524eb7e907
Fito016 <alfitof919@gmail.com>
2025-12-25 21:27:46 +0700

    update: fix code kamar

--- web/src/app/(main)/dashboard/page.tsx
+++ web/src/app/(main)/dashboard/page.tsx
@@ -8,6 +8,35 @@ export default function DashBoardPage() {
       {/*  SECTION GAMBAR KAMAR KOST */}
       <section className="px-4 py-10 lg:px-6">
          <h2 className="mb-4 text-base font-medium">Galeri Kamar Kost</h2>
+         
+         <section className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+          <KamarCard
+            image="/images/kamarkost1.jpg"
+            title="Kamar 1"
+            category="Khusus Cowok"
+            status="Tersedia"
+            price="Rp 850.000"
+            size="Single • 3 × 3 m"
+          />
+
+          <KamarCard
+            image="/images/kamarkost2.png"
+            title="Kamar 2"
+            category="Campur"
+            status="Penuh"
+            price="Rp 1.000.000"
+            size="Double • 4 × 4 m"
+          />
+
+          <KamarCard
+            image="/images/kamarkost3.png"
+            title="Kamar 3"
+            category="Khusus Cewek"
+            status="Tersedia"
+            price="Rp 900.000"
+            size="Single • 3 × 3 m"
+          />
+        </section>
       </section>
 
       {/* --- TEMPAT SECTION DATA LAINNYA --- */}
