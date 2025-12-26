5cf5c910579d6238828bdb15042503be68555d5a
Surya Agung Firdaus <ellyglobal29@gmail.com>
2025-12-26 17:11:00 +0700

    window fix

--- web/src/components/app-sidebar.tsx
+++ web/src/components/app-sidebar.tsx
@@ -180,7 +180,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
               asChild
               className="data-[slot=sidebar-menu-button]:!p-1.5"
             >
-              <a href="#">
+              <a href="/dashboard/admin">
                 <IconInnerShadowTop className="!size-5" />
                 <span className="text-base font-semibold">Dashboard Admin</span>
               </a>
