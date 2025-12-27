cfc0b3ced7ef4b39a9c6a8e51584672de02be474
Surya Agung Firdaus <ellyglobal29@gmail.com>
2025-12-27 21:51:43 +0700

    feat: create (user)

---
+++ web/src/app/(main)/dashboard/admin/profile/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { SharedProfileForm } from "@/components/ui/custom/CustomProfile";
+
+export default function AdminProfilePage() {
+  return (
+    <div className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+      <div className="flex items-center justify-between space-y-2">
+        <h2 className="text-3xl font-bold tracking-tight">Profile Admin</h2>
+      </div>
+
+      <SharedProfileForm role="admin" />
+    </div>
+  );
+}
