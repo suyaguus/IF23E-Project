966371032f1e3846a406775d870b2c7c63a86046
Fito016 <alfitof919@gmail.com>
2026-01-07 21:57:36 +0700

    fix code

--- web/src/app/(main)/dashboard/admin/page.tsx
+++ web/src/app/(main)/dashboard/admin/page.tsx
@@ -3,9 +3,9 @@ import Cards from "@/components/card";
 
 export default function DashboardAdminPage() {
   return (
-    <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+    <main className="flex flex-1 flex-col gap-4 p-4 pt-0">
       <AppSidebar />
       <Cards />
-    </div>
+    </main>
   );
 }
